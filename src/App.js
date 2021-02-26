d83855a0118e96f45ade16894d0677b0c7d828da
Joe Park <parkjs3@yahoo.com>
2021-02-26 12:33:19 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
+
+import Confirmation from "./components/confirmation";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Pizza from "./components/Pizza";
 import React from "react";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <Nav />
+      <Switch>
+        <Route path="/Pizza">
+          <Pizza />
+        </Route>
+        <Route path ="/confirmation">
+          <Confirmation />
+        </Route>
+        <Route path ="/">
+          <Home />
+        </Route>
+      </Switch>
+      
+    </Router>
   );
 };
 export default App;
